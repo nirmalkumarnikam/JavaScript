266b2635238905263848b074369bc33bfb0bff05
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-11 07:31:34 +0000

    data types

---
+++ Lecture2/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using node.js not browser
+
+// Different types of data type
+// number => 2 to power 53;
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => used to find uniqueness
+
+// object
+console.log(typeof 1)
+console.log(typeof "Nirmal")
+console.log(typeof null)  //this is error of language
+console.log(typeof undefined)  //this is error of language
