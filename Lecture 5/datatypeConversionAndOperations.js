34b5a32aa689134b7f398f9bf20ed78e610e4af1
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-11 08:48:59 +0000

    learned conversion

---
+++ Lecture 5/datatypeConversionAndOperations.js
@@ -0,0 +1,74 @@
+//try one by one
+
+// let score = "22"
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+
+
+// let score2 = "22abc"
+// console.log(typeof score2)
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log( valueInNumber2)
+
+
+
+
+// let score3 = null
+// console.log(typeof score3)
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3)
+// console.log( valueInNumber3)
+
+
+
+
+// let score4 = undefined
+// console.log(typeof score4)
+
+// let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4)
+// console.log( valueInNumber4)
+
+
+
+// let score5 = true
+// console.log(typeof score5)
+
+// let valueInNumber5 = Number(score5)
+// console.log(typeof valueInNumber5)
+// console.log( valueInNumber5)
+
+
+// "22" => 22
+// "22abc"=> NaN
+// true => 1; flase=>0
+
+
+
+
+// Boolean => anything
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Nirmal" => true  //basically isempty function
+
+
+
+
+
+// string =>anything
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
