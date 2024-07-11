17c56899371c0c5d2c4850f0c4aebac68cdab89e
Nirmalkumar <105733313+nirmalkumarnikam@users.noreply.github.com>
2024-07-11 09:20:03 +0000

    learned a lot about datatype and its conversion

--- Lecture 5/datatypeConversionAndOperations.js
+++ Lecture 5/datatypeConversionAndOperations.js
@@ -67,8 +67,64 @@
 
 
 // string =>anything
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+/*************** Operations ********************/
+let value = 3
+let negValue = -value
+// console.log(value)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 / 2)
+// console.log(2 * 2) 
+// console.log(2 % 2) 
+// console.log(2 ** 3) //power
+
+
+
+
+// let str1 = "Nirmalkumar"
+// let str2 = "Nikam"
+
+// let str3 = str1+str2
+// console.log(str3)
+
+
+
+
+//agar string first hai toh sabhi ka converstion string mein hoga..
+//agar string last mei hai toh pehle arithematiic operations honge and them string mei conversion hoga
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+4)
+// console.log(1+2+"4")
+
+
+
+
+//some tricky conversion
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 4
+
+
+
+//prefix and postfix 
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
+
+// read mdn : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
