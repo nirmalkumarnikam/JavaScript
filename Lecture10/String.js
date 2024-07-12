f0a401b7b80eb50aa05e84fa32aedf34a4dfbcdf
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-07-12 09:45:02 +0530

    some problme with vs code

---
+++ Lecture10/String.js
@@ -0,0 +1,7 @@
+const name = "Nirmal"
+const repoCount = 50
+console.log(name + repoCount + " value");  //bad Practice
+
+console.log(`Hello my name is ${name} and my repocount
+    is ${repoCount}.`);  //string interpolation (recommended)
+
